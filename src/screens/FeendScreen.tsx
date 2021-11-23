0f538d02cc44f430c35c00b28d2a641ed1c9ad45
k-1-e <obregonkevin1351@gmail.com>
2021-11-22 18:40:12 -0600

    estamos pasando parametros de una ventana a otra

--- src/screens/FeendScreen.tsx
+++ src/screens/FeendScreen.tsx
@@ -5,9 +5,13 @@ const FeedScreen = ({ navigation, route}) => {
     const { itemId, otherParam } = route.params;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Feed Screen {itemId} {otherParam}</Text>
+        <Text>Feed Screen {itemId} {otherParam} </Text>
         <Button
           title="Go to Details... again"
+          onPress={() => navigation.navigate('HomeScreen')}
+        />
+        <Button
+          title="nueva ventana"
           onPress={() => navigation.push('FeedScreen')}
         />
       </View>
--- src/screens/HomeScreens.tsx
+++ src/screens/HomeScreens.tsx
@@ -1,45 +1,43 @@
-
-import React, { useState, useEffect } from 'react';
-import { Button, Text, View } from 'react-native';
-
-
+import React, { useState, useEffect } from "react";
+import { Button, Text, View } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
-
-  const [tweetsData, setTweetsData] = useState([])
-
+  const [tweetsData, setTweetsData] = useState([]);
 
   const getTweets = async () => {
-
     const resp = await fetch('https://hidfzr.deta.dev/')
+    const data = await resp.json();
 
-    const data = await resp.json()
-
-    setTweetsData(data)
-
+    setTweetsData(data.data);
   }
   useEffect(() => {
-    getTweets()
+    getTweets();
   }, [])
 
+  
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={{ fontSize: 40, color: "orange" }}> Hola bebe </Text>
-      {/* <Button
+       {tweetsData.map(tweet  => (
+         <View key={tweet.id}>
+          <Text>{tweet.tweet_text}</Text>
+          <Button
           title="Go to Details"
           onPress={() => navigation.navigate('FeedScreen', {
-            itemId: 86,
-            otherParam: 'anything you want here',
+            id: tweet.id,
+            user_id: tweet.user_id,
+            tweet_text: tweet.tweet_text,
+            tweet_date: tweet.tweet_date,
           })}
-        /> */}
-      {
-        tweetsData.map(tweet => (
-          <Text key={tweet.id}>{tweet.tweet_text}</Text>
-        ))
-      }
-    </View>
+        />
+        </View>
+      ))
+    }
+  </View>
+    
   );
-}
+  
+};
 
-export default HomeScreen
-
+export default HomeScreen;
